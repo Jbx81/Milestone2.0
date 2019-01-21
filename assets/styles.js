ab3edc090fa94bf65fb37566fa9cb09f957cb5a3
Jonathan Ciccarella <JonCiccarella@gmail.com>
2019-01-21 15:53:31 -0500

    FINALLY posted calendar event successfully. organized file structure.

---
+++ assets/styles.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  header: {
+    fontSize: 25,
+  },
+  image: {
+    marginTop: 10,
+    width: 150,
+    height: 150,
+    borderColor: 'rgba(0,0,0,0.2)',
+    borderWidth: 3,
+    borderRadius: 100,
+  },
+});
+
+export default styles;
