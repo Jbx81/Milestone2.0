de17d613620345043b8c2f2b61939befddc0ba31
Jonathan Ciccarella <JonCiccarella@gmail.com>
2019-01-21 19:21:33 -0500

    first commit for homePage

--- App.js
+++ App.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import Expo from 'expo';
-import { View } from 'react-native';
+import { View, Text } from 'react-native';
 import GoogleAuth from './client/GoogleAuth';
+import styles from './assets/styles';
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      event: {},
+    };
   }
 
   render() {
     return (
-      <View>
+      <View style={styles.container}>
+        <View style={styles.welcome}>
+          <Text>Hello World</Text>
+        </View>
         <GoogleAuth />
       </View>
     );
