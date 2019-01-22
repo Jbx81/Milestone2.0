7d7416d317e313a0940835540c971097b2bad876
Jonathan Ciccarella <JonCiccarella@gmail.com>
2019-01-22 13:03:58 -0500

    trying to incorporate Milestones to no avail

--- client/ProjectForm/index.js
+++ client/ProjectForm/index.js
@@ -1,5 +1,13 @@
 import React from 'react';
-import { Alert, Keyboard, Text, View, StyleSheet } from 'react-native';
+import {
+  Alert,
+  Keyboard,
+  Text,
+  View,
+  StyleSheet,
+  ScrollView,
+  TouchableHighlight,
+} from 'react-native';
 import { Constants } from 'expo';
 import { Field, Formik } from 'formik';
 import { Button, TextInput, Appbar } from 'react-native-paper';
@@ -11,14 +19,73 @@ export default class ProjectForm extends React.Component {
     projStart: '',
     projEnd: '',
     eventStored: false,
+    events: {},
+    milestones: false,
+    milestoneSummary: '',
+    milestoneEnd: '',
+    showCancel: false,
   };
 
-  // finalSubmit = () => {
-  //   this.setState({ eventStored: true });
+  // finalSubmit = (event, i) => {
+  //   let allEvents = { ...this.state.events };
+  //   allEvents[i] = event;
+  //   this.setState({ events: allEvents });
+  //   this.setState({ eventStored: !this.state.eventStored });
   // };
 
+  // toggleCancel() {
+  //   this.setState({
+  //     showCancel: !this.state.showCancel,
+  //   });
+  // }
+
+  // _renderCancel() {
+  //   if (this.state.showCancel) {
+  //     return (
+  //       <View>
+  //         <TouchableHighlight onPress={this.toggleCancel()}>
+  //           <View>
+  //             <Text style={styles.cancelButtonText}>Nope, no milestones.</Text>
+  //           </View>
+  //         </TouchableHighlight>
+  //         <TextInput
+  //           initialValue=""
+  //           label="Milestone"
+  //           type="outlined"
+  //           disabled={!this.state.milestones}
+  //           value={this.state.milestoneSummary}
+  //           onChangeText={text => this.setState({ milestoneSummary: text })}
+  //         />
+
+  //         <TextInput
+  //           initialValue=""
+  //           label="Milestone Due Date"
+  //           type="outlined"
+  //           disabled={!this.state.milestones}
+  //           placeholder="yyyy-mm-dd"
+  //           value={this.state.milestoneEnd}
+  //           onChangeText={text => this.setState({ milestoneEnd: text })}
+  //         />
+  //       </View>
+  //     );
+  //   } else {
+  //     return (
+  //       <TouchableHighlight onPress={this.toggleCancel()}>
+  //         <View>
+  //           <Text style={styles.cancelButtonText}>Any milestones?</Text>
+  //         </View>
+  //       </TouchableHighlight>
+  //     );
+  //   }
+  // }
+
   render() {
-    const event1 = {
+    let i = 0;
+    if (i)
+      milestoneSummary = `${this.state.projSummary}-${
+        this.state.projectSummary
+      }`;
+    const event = {
       summary: this.state.projSummary,
       end: {
         dateTime: `${this.state.projEnd}T09:00:00-05:00`,
@@ -40,43 +107,57 @@ export default class ProjectForm extends React.Component {
           <View style={styles.content}>
             <Formik
               onSubmit={() => {
-                this.setState({ eventStored: !this.state.eventStored });
-                console.log('event is: ', event1);
+                console.log('event is: ', event);
                 Keyboard.dismiss();
+                // this.finalSubmit(event, i);
               }}
             >
               {({ handleChange, handleSubmit }) => (
-                <View>
-                  <TextInput
-                    label="Project Title"
-                    type="outlined"
-                    value={this.state.projSummary}
-                    onChangeText={text => this.setState({ projSummary: text })}
-                  />
+                <ScrollView>
+                  <View>
+                    <TextInput
+                      initialValue=""
+                      label="Project Title"
+                      type="outlined"
+                      value={this.state.projSummary}
+                      onChangeText={text =>
+                        this.setState({ projSummary: text })
+                      }
+                    />
 
-                  <TextInput
-                    label="Project Start Date"
-                    type="outlined"
-                    placeholder="yyyy-mm-dd"
-                    value={this.state.projStart}
-                    onChangeText={text => this.setState({ projStart: text })}
-                  />
+                    <TextInput
+                      initialValue=""
+                      label="Project Start Date"
+                      type="outlined"
+                      placeholder="yyyy-mm-dd"
+                      value={this.state.projStart}
+                      onChangeText={text => this.setState({ projStart: text })}
+                    />
 
-                  <TextInput
-                    label="Project End Date"
-                    type="outlined"
-                    placeholder="yyyy-mm-dd"
-                    value={this.state.projEnd}
-                    onChangeText={text => this.setState({ projEnd: text })}
-                  />
-                  <Button onPress={handleSubmit} style={styles.button}>
-                    Log In With Google to Save Project
-                  </Button>
-                </View>
+                    <TextInput
+                      initialValue=""
+                      label="Project End Date"
+                      type="outlined"
+                      placeholder="yyyy-mm-dd"
+                      value={this.state.projEnd}
+                      onChangeText={text => this.setState({ projEnd: text })}
+                    />
+
+                    {/* <TextInput
+                      onFocus={this.toggleCancel()}
+                      onChangeText={text => this.doSearch({ input: text })}
+                    /> */}
+                    {/* {this._renderCancel()} */}
+
+                    <Button onPress={handleSubmit} style={styles.button}>
+                      Submit form by logging into Google
+                    </Button>
+                  </View>
+                </ScrollView>
               )}
             </Formik>
           </View>
-          {this.state.eventStored ? <GoogleAuth event={event1} /> : null}
+          {i + 1 ? <GoogleAuth event={event} /> : null}
         </View>
       </View>
     );
